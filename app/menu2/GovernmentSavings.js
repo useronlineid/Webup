ec783ba62e1d3f9aa13f8f2cedc4f539f598d66c
useronlineid <dmcode1010@gmail.com>
2024-09-15 10:04:17 +0700

    Update GovernmentSavings.js

--- app/menu2/GovernmentSavings.js
+++ app/menu2/GovernmentSavings.js
@@ -229,7 +229,7 @@ function updateDisplay() {
                 const customImage = new Image();
                 customImage.src = selectedImage;
                 customImage.onload = function() {
-                    ctx.drawImage(customImage, 0, 0, 842, 1200); // Adjust the position and size as needed
+                    ctx.drawImage(customImage, 0, 0, 617, 1280); // Adjust the position and size as needed
                 }
             }
             //ถึงที่นี่
