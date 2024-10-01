859ea8b89cfdc83fe39a5fe40b8a8ac2f1c4074a
useronlineid <dmcode1010@gmail.com>
2024-10-01 12:54:27 +0700

    Update slipktb.js

--- app/menu2/slipktb.js
+++ app/menu2/slipktb.js
@@ -223,7 +223,7 @@ function updateDisplay() {
                 const customImage = new Image();
                 customImage.src = selectedImage;
                 customImage.onload = function() {
-                    ctx.drawImage(customImage, 0, 0, 945, 1200); // Adjust the position and size as needed
+                    ctx.drawImage(customImage, 0, 0, 1008, 1280); // Adjust the position and size as needed
                 }
             }
             //ถึงที่นี่
