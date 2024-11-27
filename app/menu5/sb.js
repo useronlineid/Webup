0b4a3f44b7b691e43ff43b5a678ffea0bc8eb5a7
useronlineid <dmcode1010@gmail.com>
2024-11-27 16:43:46 +0700

    Update sb.js

--- app/menu5/sb.js
+++ app/menu5/sb.js
@@ -119,7 +119,7 @@ function updateDisplay() {
     const ctx = canvas.getContext('2d');
     
     const backgroundImage = new Image();
-    backgroundImage.src = '../assets/image/paper/sb.jpg';
+    backgroundImage.src = '../assets/image/paper/sb1.jpg';
     backgroundImage.onload = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
