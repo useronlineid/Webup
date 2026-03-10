96a16aeae920aca7b421a62a8d2ee3a7a0b5a6b6
useronlineid <dmcode1010@gmail.com>
2026-03-10 12:06:07 +0700

    Update slipktb.js

--- app/menu2/slipktb.js
+++ app/menu2/slipktb.js
@@ -302,7 +302,7 @@ function updateDisplay() {
             drawText(ctx, `${senderaccount}`,188, 572,37, 'DXKrungthaiMedium', '#586970', 'left', 1.5, 1, 0, 0, 500,-1.6);
             
             drawText(ctx, `SCB มณี SHOP (${receivername})`, 188,677.2,47, 'DXKrungthaiBold', '#000000', 'left', 1.5, 3, 0, 0, 800, -0.6);
-            drawText(ctx, `${receiveraccount}`, 188, 734.4,37, 'DXKrungthaiMedium', '#586970', 'left', 1.5, 1, 0, 0, 500,-1.6);
+            drawText(ctx, `(${receiveraccount})`, 188, 734.4,37, 'DXKrungthaiMedium', '#586970', 'left', 1.5, 1, 0, 0, 500,-1.6);
             drawText(ctx, `${Itemcode}`, 963.7, 830,38.5, 'DXKrungthaiMedium', '#000000', 'right', 1.5, 2, 0, 0, 500, 0);
             // ลบช่องว่างทั้งหมดใน receivername
             const modifiedReceiverName = receivername.replace(/\s+/g, '');
